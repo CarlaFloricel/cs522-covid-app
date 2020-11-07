3f300172c22c9bc0d074bc276bc312ae920d74cc
carla_floricel <carla.floricel@gmail.com>
2020-11-07 16:56:52 -0500

    components routing enabled

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
 import HomePage from './components/homepage/homepage.component';
 import Activities from './components/activities/activities.component';
 import Vaccines from './components/vaccines/vaccines.component';
 import Faq from './components/faq/faq.component';
 import Map from './components/map/map.component';
 
+
 function App() {
   return (
     <div className="App">
-      <Faq/>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/map' component={Map}></Route>
+        <Route exact path='/vaccines' component={Vaccines}></Route>
+        <Route exact path='/activities' component={Activities}></Route>
+        <Route exact path='/faq' component={Faq}></Route>
+      </Switch>
+
     </div>
   );
 }
 
 export default App;
+
--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -1,31 +1,58 @@
 import React from 'react';
 import './homepage.styles.css';
 
-const HomePage = () => (
-    <div className='homepage'>
-        <div className='directory-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>menu item</h1>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>menu item</h1>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>menu item</h1>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>menu item</h1>
-                </div>
+class HomePage extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            menuItems: [
+               {
+                   title: "Safe Travel",
+                   imageUrl: '',
+                   id: 1,
+                   url: '/map'
+               },
+               {
+                    title: "Vaccines",
+                    imageUrl: '',
+                    id: 2,
+                    url: '/vaccines'
+                },
+                {
+                    title: "Daily Activity",
+                    imageUrl: '',
+                    id: 3,
+                    url: '/activities'
+                },
+                {
+                    title: "FAQ",
+                    imageUrl: '',
+                    id: 4,
+                    url: '/faq'
+                },
+            ]
+        }
+    }
+
+
+    render(){
+        return(  
+        <div className='homepage'>
+            <div className='directory-menu'>
+                {
+                this.state.menuItems.map( item => (
+                    <div className='menu-item' key={item.id}>
+                        <div className='content'>
+                            <button key={item.id} type="button" className="btn btn-info btn-lg" 
+                                onClick={() => this.props.history.push(`${item.url}`)}>
+                            {item.title}
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-
-    </div>
-)
+        )
+    }
+}
 export default HomePage;
