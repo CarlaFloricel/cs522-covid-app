b36e954a37e391f8d44e8d0887e7ee55218ff9a1
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2020-11-16 17:47:59 -0600

    update made on vaccines

--- src/components/vaccines/vaccines.d3.component.js
+++ src/components/vaccines/vaccines.d3.component.js
@@ -1,49 +1,72 @@
 import * as d3 from 'd3'
+import './vaccines.styles.css';
 
-const url = 'https://udemy-react-d3.firebaseio.com/ages.json'
-
+// const url = 'https://udemy-react-d3.firebaseio.com/ages.json'
+const vaccines = require('./vaccines.json')
+const domainX = ["May 20", "Jun 20", "Jul 20", "Aug 20", "Sep 20", "Oct 20", "Nov 20", "Dec 20",  "Jan 21", "Feb 21", "Mar 21", "Apr 21", "May 21", "Jun 21"]
+const domainY = ["Inactivated Virus", "Life Attenuated Virus", "Protein Subnit", "DNA-Based", "RNA-Based", "Replicating Viral Vector", "Non-Replicating Viral Vector", "Virus-like Particle", "Other Vaccines"]
 export default class D3DummyChart {
     constructor(element) {
         const svg = d3.select(element)
             .append("svg")
-            .attr("width", 500)
-            .attr("height", 250)
+            .attr("width", 825)
+            .attr("height", 300)
 
         let xScale = d3.scalePoint()
-                        .domain(["May 2019", "Dec 2019", "Jun 2021"])
-                        .range([30, 450])
+                        .domain(domainX)
+                        .range([150, 800])
 
         let xAxis = d3.axisBottom()
                     .scale(xScale)
-                    .ticks(3)
+                    // .ticks(3)
 
          svg.append("g")
                     .call(xAxis)
-                    .attr("transform", "translate(0," + (225) +")")
-        
+                    .attr("transform", "translate(0," + (280) +")")
+
+        svg.append("g")         
+            .attr("class", "grid")
+            .attr("transform", "translate(0," + 280 + ")")
+            .call(make_x_axis()
+                .tickSize(-300, 0, 0)
+                .tickFormat("")
+            )
+
         let yScale = d3.scaleBand()
-                        .domain(['V1','V2','V3','V4','V5'])
-                        .range([0, 230])
+                        .domain(domainY)
+                        .range([0, 270])
                         .padding(0.1)
         let yAxis = d3.axisLeft()
                      .scale(yScale)
 
-        svg.append("g").call(yAxis).attr("transform", "translate(25,0)")
-        
-        d3.json(url).then(agesData => {
+        svg.append("g").call(yAxis).attr("transform", "translate(150,0)")
+            .selectAll("text")	            
+            // .attr("transform", "rotate(-20)");
 
-            const rects = svg.selectAll("rect")
-                .data(agesData)
-
+        const group = svg.append("g")
+        for(let item of vaccines){
+            const rects = group.selectAll('rect')
+                .data(vaccines)
             rects.enter()
                 .append('rect')
-                .attr("x", 30)
-                .attr("y", (d,i) => i * 50 )
-                .attr("width", d => d.age * 20)
+                .attr("x", (d) => {return xScale(d["starting-date"])})
+                .attr("y", (d) => {return yScale(d.name)})
+                .attr("width", (d) => {return (xScale(d["ending-date"]) - xScale(d["starting-date"]))})
                 .attr("height", 20)
                 .attr("fill", "green")
+        }
 
 
-        })
+        function make_x_axis() {    
+            let xScale = d3.scalePoint()
+                        .domain(domainX)
+                        .range([150, 800])
+
+            return d3.axisBottom()
+                .scale(xScale)
+        }
+
     }
+
+    
 }
